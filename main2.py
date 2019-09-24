8656ecf590b13c428bcf3c67ef2922b19028d346
Mateusz Grzywacz <amateusz.grzywacz@gmail.com>
2019-09-24 21:52:04 +0200

    fuck me. fixed colour mixing, tiles no aligning (integer division propagation)

--- main2.py
+++ main2.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk, ImageColor
 import tkinter as tk
 from tkinter import filedialog
 from math import sqrt
@@ -47,29 +47,26 @@ def create_image(filename, width, height):
         return img.resize((int(w * scale), int(h * scale)), Image.ANTIALIAS)
 
 
-class Home(object):
+class HomeWindow():
     """
     master: tk.Tk window.
     screen: tuple, (width, height).
     """
 
-    def __init__(self, master, screen):
-        self.screen = w, h = screen
-        self.master = master
+    def __init__(self, master, canvas_size, filenames=None):
+        print(canvas_size)
+        self.canvas_size = canvas_size  # policy: everything stretched to fill this size. no aspect preservation
+        self.parent = master
 
-        self.frame = tk.Frame(self.master)
+        self.frame = tk.Frame(self.parent)
         self.frame.pack()
-        self.can = tk.Canvas(self.frame, width=w, height=h)
-        self.can.pack()
+        self.canvas = tk.Canvas(self.frame, width=self.canvas_size[0], height=self.canvas_size[1])
+        self.canvas.pack()
 
-        self.fname1 = filedialog.askopenfilename()
-        self.fname2 = filedialog.askopenfilename()
-        self.image1 = create_image(self.fname1, w, h)
-        self.image2 = create_image(self.fname2, w, h)
-        self.image3 = create_image(self.fname2, w, h)
+        self.loadImages(filenames)
 
         # Center of screen.
-        self.center = w // 2, h // 2
+        self.center = self.canvas_size[0] // 2, self.canvas_size[1] // 2
         # Start with no photo on the screen.
         self.photo = False
 
@@ -77,7 +74,7 @@ class Home(object):
         self.draw()
 
         # Key bindings.
-        self.master.bind("<Return>", self.reset)
+        self.parent.bind("<Return>", self.reset)
         # self.master.bind("<Motion>", self.erase)
 
         self.pos = []
@@ -87,20 +84,20 @@ class Home(object):
 
         # declare a heatmap
         self.map = [
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0.2, 0.2, 0, 0],
-            [0, 0, 0.2, 0.2, 0, 0],
-            [0, 0, 0.4, 0.4, 0, 0],
-            [0, 0, 0.4, 0.4, 0, 0],
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
+            [0.1, 0.1, 0.40, 0.60, 0.1, 0.1],
+            [0.1, 0.1, 0.30, 0.50, 0.1, 0.1],
+            [0.1, 0.1, 0.35, 0.45, 0.1, 0.1],
+            [0.1, 0.1, 0.35, 0.40, 0.1, 0.1],
+            [0.1, 0.1, 0.30, 0.35, 0.1, 0.1],
+            [0.1, 0.1, 0.20, 0.30, 0.1, 0.1],
+            [0.1, 0.1, 0.15, 0.25, 0.1, 0.1],
+            [0.1, 0.1, 0.15, 0.20, 0.1, 0.1],
+            [0.1, 0.1, 0.10, 0.10, 0.1, 0.1],
         ]
 
         # size of heatmap tiles in pixels based on self.map 2d list and size of an image
-        self.heat_w = self.screen[0] // len(self.map[0])
-        self.heat_h = self.screen[1] // len(self.map)
+        self.heat_tile_w = self.canvas_size[0] / len(self.map[0])
+        self.heat_tile_h = self.canvas_size[1] / len(self.map)
 
         # value of targeted areas
         self.mask = 0
@@ -108,19 +105,30 @@ class Home(object):
         # mask_list populates with indexes of matching values with self.mask after erase function is called
         self.mask_list = []
         self.heat()
-        self.erase(0, 0)
+        self.blur_action(0, 0)
+
+    def loadImages(self, filenames=None):
+        if not filenames or len(filenames) != 2:
+            fname1 = filedialog.askopenfilename()
+            fname2 = filedialog.askopenfilename()
+        else:
+            fname1 = filenames[0]
+            fname2 = filenames[1]
+        self.image_clear = create_image(fname1, width=self.canvas_size[0], height=self.canvas_size[1])
+        self.image_working = create_image(fname2, width=self.canvas_size[0], height=self.canvas_size[1])
+        self.image_blurred = create_image(fname2, width=self.canvas_size[0], height=self.canvas_size[1])
 
     def draw(self):
         """
         If there is a photo on the canvas, destroy it.
-        Draw self.image2 on the canvas.
+        Draw self.image_working on the canvas.
         """
         if self.photo:
-            self.can.delete(self.photo)
+            self.canvas.delete(self.photo)
             self.label.destroy()
 
-        p = ImageTk.PhotoImage(image=self.image2)
-        self.photo = self.can.create_image(self.center, image=p)
+        p = ImageTk.PhotoImage(image=self.image_working)
+        self.photo = self.canvas.create_image(self.center, image=p)
         self.label = tk.Label(image=p)
         self.label.image = p
 
@@ -128,10 +136,10 @@ class Home(object):
     def reset(self, event):
         """ Enter/Return key. """
         self.frame.destroy()
-        self.__init__(self.master, self.screen)
+        self.__init__(self.parent, self.canvas_size)
 
     # def erase(self, event):
-    def erase(self, a, b):
+    def blur_action(self, a, b):
         """
         Mouse motion binding.
         Erase part of top image (self.photo2) at location (event.x, event.y),
@@ -145,14 +153,14 @@ class Home(object):
         n = 60
         # mask_list = [(i for i, x in enumerate(self.map)) if x == self.mask]
 
-        for j in range(len(self.map)):
-            for i, x in enumerate(self.map[j]):
-                if x == self.mask:
-                    self.mask_list.append([i, j])
+        for vertical in range(len(self.map)):
+            for i, horizontal in enumerate(self.map[vertical]):
+                if horizontal == self.mask:
+                    self.mask_list.append([i, vertical])
         print(self.mask_list)
         for coords in self.mask_list:
-            a = coords[0] * self.heat_w + self.heat_w // 2
-            b = coords[1] * self.heat_h + self.heat_h // 2
+            a = int(coords[0] * self.heat_tile_w + self.heat_tile_w / 2)
+            b = int(coords[1] * self.heat_tile_h + self.heat_tile_h / 2)
             for x in range(a - r, a + r + 1):
                 for y in range(b - r, b + r + 1):
                     try:
@@ -179,8 +187,8 @@ class Home(object):
                                 '''
                         if (x - a) * (x - a) + (y - b) * (
                                 y - b) <= r * r * random() ** 2:  # version with random gradient
-                            p = self.image1.getpixel((x, y))
-                            self.image2.putpixel((x, y), p)
+                            p = self.image_clear.getpixel((x, y))
+                            self.image_working.putpixel((x, y), p)
 
                     except IndexError:
                         pass
@@ -195,8 +203,8 @@ class Home(object):
                                 y - self.pos[0][1]) <= r * r
                                 and random() > 0.7
                         ):
-                            q = self.image3.getpixel((x, y))
-                            self.image2.putpixel((x, y), q)
+                            q = self.image_blurred.getpixel((x, y))
+                            self.image_working.putpixel((x, y), q)
                     except IndexError:
                         pass
             self.pos.pop(0)
@@ -209,30 +217,41 @@ class Home(object):
         if 'alpha' in kwargs:
             alpha = int(kwargs.pop('alpha') * 255)
             fill = kwargs.pop('fill')
-            fill = self.master.winfo_rgb(fill) + (alpha,)
-            image = Image.new('RGBA', (x2 - x1, y2 - y1), fill)
+            print(self.parent.winfo_rgb(fill))
+            fill_mixed = ImageColor.getrgb(fill) + (alpha,)
+            image = Image.new('RGBA', (x2 - x1, y2 - y1), fill_mixed)
             self.images.append(ImageTk.PhotoImage(image))
-            self.can.create_image(x1, y1, image=self.images[-1], anchor='nw')
-        self.can.create_rectangle(x1, y1, x2, y2, **kwargs)
+            self.canvas.create_image(x1, y1, image=self.images[-1], anchor='nw')
+        self.canvas.create_rectangle(x1, y1, x2, y2, **kwargs)
 
     # draw a heatmap
     def heat(self):
         for h in range(len(self.map)):
             for w in range(len(self.map[0])):
-                color = Color(hsl=(self.map[h][w], 1, 0.5))
-                self.create_rectangle(w * self.heat_w, h * self.heat_h,
-                                      w * self.heat_w + self.heat_w, h * self.heat_h + self.heat_h, fill="%s" % color,
-                                      alpha=.5)
+                color = Color(hsl=(self.map[h][w], .9, 0.5))
+                self.create_rectangle(int(w * self.heat_tile_w), \
+                                      int(h * self.heat_tile_h), \
+                                      int(w * self.heat_tile_w + self.heat_tile_w), \
+                                      int(h * self.heat_tile_h + self.heat_tile_h), \
+                                      fill="%s" % color, alpha=.5)
 
 
-def main(screen=(720, 1024)):
+def main(window_size=None):
     root = tk.Tk()
-    root.resizable(0, 0)
-    Home(root, screen)
+    if not window_size:  # root.attributes('-zoomed', True)
+        width, height = root.wm_maxsize()  # == root.winfo_screenwidth()
+    else:
+        width, height = window_size
+    # root.attributes('-fullscreen', True)
+    # root.resizable(0, 0)
+    root.geometry(f'{width}x{height}+{root.wm_maxsize()[0] - width}+0')
+    root.update()  # no mainloop yet
+    root.title('mgr Alka Czekaja. ten program: Eryk Czekaj × Mateusz Grzywacz')
+    blur_app = HomeWindow(root, (root.winfo_width(), root.winfo_height()), ['1.jpg', '2.jpg'])
     root.mainloop()
 
 
 if __name__ == '__main__':
-    main()
+    main((500, 800))
 
 # amateusz has changes a lot… (test)
